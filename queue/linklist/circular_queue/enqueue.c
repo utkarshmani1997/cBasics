69c5cdadcfbdde4338c9a1e8252e00caf6ad03b9
Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>
2018-10-03 20:25:14 +0530

    Add circular queue operations
    Signed-off-by: Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>

---
+++ queue/linklist/circular_queue/enqueue.c
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  enqueue.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  Tuesday 25 September 2018 02:55:02  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Utkarsh Mani Tripathi
+ *   Organization:  cBasics
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include "queue.h"
+int length=0;
+// enqueue is used to push the data into queue
+void enqueue(int random) {
+    printf("enqueue %d to queue\n", random);
+    if (front == NULL) {
+        node = malloc(sizeof(struct queueNode));
+        if (!check(node)){
+            return;
+        }
+        rear = node;
+    }
+    else {
+        node->next = malloc(sizeof(struct queueNode));
+        if (!check(node)){
+            return;
+        }
+        node = node->next;
+    }
+    node->data = random;
+    node->next = rear;
+    front = node;
+    length++;
+}
---
+++ queue/linklist/circular_queue/queue.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include "queue.h"
+
+struct queueNode *node, *ptr, *rear=NULL, *front=NULL;
+
+int main() {
+    int i, data;
+    for (i = 0; i < 5; i++) {
+        data = rand();
+        enqueue(data);
+    }
+    printf("length of queue %d\n", length);
+    for (i = 0; i < 6; i++) {
+        peek();
+        dequeue();
+    }
+    enqueue(rand());
+    enqueue(rand());
+    enqueue(rand());
+    dequeue();
+    dequeue();
+    peek();
+
+    printf("length of queue %d\n", length);
+    return 0;
+}
