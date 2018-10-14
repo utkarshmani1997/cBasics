f84f606518c01419e8fa120a3f512df5228fdf0a
Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>
2018-10-14 09:03:08 +0530

    fix the diff calculation logic
    Signed-off-by: Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>

--- time/time.c
+++ time/time.c
@@ -5,11 +5,15 @@
 int main ()
 {
     struct timespec start, end;
+    time_t now;
+    // time since 1 jan 1970 in seconds
+    now = time(NULL);
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     sleep(5);
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
 
-    double diff =  (end.tv_sec - start.tv_sec);
-    printf("diff: %f, start: %lld, end: %lld\n", (double)delta_us, (long long)start.tv_sec, (long long)end.tv_sec);
+    // get the difference in nanoseconds
+    double diff = (end.tv_sec - start.tv_sec)*1000000000 + (end.tv_nsec - start.tv_nsec);
+    printf("diff: %f, start: %lld, end: %lld, now: %lld\n", (double)diff, (long long)start.tv_sec, (long long)end.tv_sec, (long long)now);
     return 0;
 }
